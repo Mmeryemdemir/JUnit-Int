894433a95e4aa34ad420b277f12d1650cf30371f
Meryem <mrymdmr199@gmail.com>
2022-10-18 00:29:17 +0300

    ders08End

--- src/test/java/ders08_İframe_windows/C02_HandleWindows.java
+++ src/test/java/ders08_İframe_windows/C02_HandleWindows.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.Set;
 
 public class C02_HandleWindows {
 
@@ -30,8 +31,36 @@ public class C02_HandleWindows {
         // https://the-internet.herokuapp.com/iframe adresine gidin
         driver.get("https://the-internet.herokuapp.com/iframe");
 
+        String ilkSayfaWindowHandDeğeri = driver.getWindowHandle();
         // elemental selenium linkini tiklayin
         driver.findElement(By.linkText("Elemental Selenium")).click();
+
+        /*
+        Bir linke tıkladığımızda driver'ı acılan yeni window'a direk switch yapamayız
+        öncelikle o sayfanın window handle değeri bulmalı
+        ve o değeri kullanarak yeni sayfaya geçmeliyiz
+
+
+       ilk sayfa WHD : CDwindow-28627133898E14448A33733211745B19
+
+       Iki window’un window handle degerleri :
+       [CDwindow-28627133898E14448A33733211745B19, CDwindow-16D881C607BD3D31BF6C8B124051C4A5]
+         */
+
+        Set<String> windowHandlesSeti = driver.getWindowHandles();
+        String ikinciSayfaWinHandleDeğeri="";
+
+        for (String eachHandleDeğer:windowHandlesSeti
+             ) {
+            if (!eachHandleDeğer.equals(ilkSayfaWindowHandDeğeri)){
+                ikinciSayfaWinHandleDeğeri=eachHandleDeğer;
+            }
+        }
+
+        driver.switchTo().window(ikinciSayfaWinHandleDeğeri);
+
+        System.out.println("İlk sayfa WHD : " + ilkSayfaWindowHandDeğeri);
+        System.out.println("İki window'un window handle değerleri :" + windowHandlesSeti);
 
         // Acilan sayfadaki en bastaki yazinin gorunur oldugu
 
@@ -52,6 +81,9 @@ public class C02_HandleWindows {
 
        bir test sırasında birden fazla window acılıyorsa
        driver'a bu window'lar arasında geçiş yaptırabiliriz
+
+
+
         */
     }
 
